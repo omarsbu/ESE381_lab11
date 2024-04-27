6e6be2b520dbf61c1a1e530fef72b9ecf57e1166
omarsbu <99481191+omarsbu@users.noreply.github.com>
2024-04-27 18:56:03 -0400

    Update avr128db48_i2c.c

--- avr128db48_i2c.c
+++ avr128db48_i2c.c
@@ -1,21 +1,63 @@
-#ifndef AVR128DB48_I2C_H_
-#define AVR128DB48_I2C_H_
-
-#define F_CPU 4000000     // 4MHz default clock
-#define CLK_PER 4000000   // 4MHz default clock
-
-#include <avr/io.h>
-#include <util/delay.h>
+#include "avr128db48_i2c.h"
 
 /* AVR128DB48 I2C Driver Functions */
-static void wait_for_WIF(void);
-static void wait_for_RIF(void);
-static void wait_for_idle(void);
-static void issue_stop_condition(void);
-static void issue_ack(void);
-static void issue_nack(void);
-static void i2c0_transmit_address_packet(uint8_t address, uint8_t direction_bit);
-static void i2c0_send_data_packet(uint8_t data);
-static uint8_t i2c0_receive_data_packet(void);
+void wait_for_WIF(void)
+{
+	// Write interrupt flag (WIF), wait until transmit address or byte write operation is completed
+	while(!(TWI0.MSTATUS & TWI_WIF_bm));
+}
 
-#endif /* AVR128DB48_I2C_H_ */
+void wait_for_RIF(void)
+{
+	// Read interrupt flag (RIF), wait until read operation is successfully completed
+	while(!(TWI0.MSTATUS & TWI_RIF_bm));
+}
+
+void wait_for_idle(void)
+{
+	// Wait until bus state is idle
+	while((TWI0.MSTATUS & TWI_BUSSTATE_BUSY_gc) != TWI_BUSSTATE_IDLE_gc) ;	
+}
+
+void issue_stop_condition(void)
+{
+	// Issue stop condition
+	TWI0.MCTRLB |= TWI_MCMD_STOP_gc;	
+}
+
+void issue_ack(void)
+{
+	// MCMD - Issue ACK followed by read operation
+	TWI0.MCTRLB = 0x02;
+}
+
+void issue_nack(void)
+{
+	// MCMD - Issue NACK followed by stop condition
+	TWI0.MCTRLB = 0x07;
+}
+
+void i2c0_transmit_address_packet(uint8_t address, uint8_t direction_bit)
+{
+	// Write -> direction_bit = 0; Read -> direction_bit = 1
+	TWI0.MADDR = ((address << 1) | (direction_bit & 0x01));
+	
+	if ((direction_bit & 0x01) == 0x00)
+		wait_for_WIF();	// Wait for write interrupt flag (WIF)
+	else
+		wait_for_RIF();	// Wait for read interrupt flag (RIF)
+}
+
+void i2c0_send_data_packet(uint8_t data)
+{
+	/* Stop bit should be issued after data is sent */
+	TWI0.MDATA = data;
+	wait_for_WIF();
+}
+
+uint8_t i2c0_receive_data_packet(void)
+{
+	/* ACK or NACK should be issued after this function */
+	wait_for_RIF();
+	return TWI0.MDATA;	// Reading MDATA register clears RIF flag
+}
