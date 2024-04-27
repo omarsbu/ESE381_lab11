a0aeafcf1b55d3893e4e38aca2d82e7a36d31eda
omarsbu <99481191+omarsbu@users.noreply.github.com>
2024-04-27 18:55:44 -0400

    Update avr128db48_i2c.c

--- avr128db48_i2c.c
+++ avr128db48_i2c.c
@@ -1,14 +1,21 @@
-#include "avr128db48_i2c.h"
+#ifndef AVR128DB48_I2C_H_
+#define AVR128DB48_I2C_H_
+
+#define F_CPU 4000000     // 4MHz default clock
+#define CLK_PER 4000000   // 4MHz default clock
+
+#include <avr/io.h>
+#include <util/delay.h>
 
 /* AVR128DB48 I2C Driver Functions */
-void wait_for_WIF(void)
-{
-	// Write interrupt flag, wait until transmit address or byte write operation is completed
-	while(!(TWI0.MSTATUS & TWI_WIF_bm));
-}
+static void wait_for_WIF(void);
+static void wait_for_RIF(void);
+static void wait_for_idle(void);
+static void issue_stop_condition(void);
+static void issue_ack(void);
+static void issue_nack(void);
+static void i2c0_transmit_address_packet(uint8_t address, uint8_t direction_bit);
+static void i2c0_send_data_packet(uint8_t data);
+static uint8_t i2c0_receive_data_packet(void);
 
-void wait_for_RIF(void)
-{
-	// Read interrupt flag, wait until read operation is successfully completed
-	while(!(TWI0.MSTATUS & TWI_RIF_bm));
-}
+#endif /* AVR128DB48_I2C_H_ */
